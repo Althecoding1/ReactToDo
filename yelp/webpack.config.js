7714b26c060c8ffae426e98663c8416fa7fd6918
Arthur Livingston <Artielivingston1@gmail.com>
2016-12-31 12:54:59 -0800

    Fixed syntax error in webpack config file

--- yelp/webpack.config.js
+++ yelp/webpack.config.js
@@ -1,16 +1,18 @@
+const NODE_ENV = process.env.NODE_ENV;
+
 const webpack = require('webpack');
 const fs = require('fs');
 const path = require('path'),
       join = path.join,
       resolve = path.resolve;
+
 const getConfig = require('hjs-webpack');
-const NODE_ENV = process.env.NODE_ENV;
 const isDev = NODE_ENV === 'development';
 
 const root = resolve(__dirname);
 const src = join(root, 'src');
 const modules = join(root, 'node_modules');
-const dest = join(rootm, 'dist');
+const dest = join(root, 'dist');
 
 var config = getConfig({
   isDev: isDev,
