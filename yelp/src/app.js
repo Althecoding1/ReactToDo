6310c244413c8f04125bf87d0b9ea1f983977986
Arthur Livingston <Artielivingston1@gmail.com>
2017-01-02 16:36:26 -0800

    add new style sheet for testing webpack build

--- yelp/src/app.js
+++ yelp/src/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './app.css'
+import './app.css';
+import styles from './styles.module.css';
 
 const App = React.createClass({
   render: function() {
-    return (<div><h1>Hey! there Artie</h1></div>);
+    return(
+      <div className={styles.wrapper}>
+        Text text text
+      </div>
+    )
   }
 });
 
