45e40bcb768d4c1cad755ff5841fe783c32f8d1b
Arthur Livingston <Artielivingston1@gmail.com>
2017-01-02 22:31:32 -0800

    update to node evironment in webpack

--- yelp/webpack.config.js
+++ yelp/webpack.config.js
@@ -1,5 +1,21 @@
 const NODE_ENV = process.env.NODE_ENV;
 
+const dotenv = require('dotenv');
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
+});
+const envVariables = Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines = Object.keys(envVariables).reduce((memo, key) => {
+  const val = JSON.stringify(envVariables[key]);
+  memo[`__${key.toUpperCase()}__`] = val;
+  return memo;
+}, {
+  __NODE_ENV__: JSON.stringify(NODE_ENV)
+});
+
 const webpack = require('webpack');
 const fs = require('fs');
 const path = require('path'),
@@ -26,6 +42,10 @@ config.postcss = [].concat([
   require('autoprefixer')({}),
   require('cssnano')({})
 ]);
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins);
 
 const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
 
