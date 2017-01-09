59b8387b66ff06fcc92306b51a7bcca396edd5e0
Arthur Livingston <Artielivingston1@gmail.com>
2017-01-08 20:28:04 -0800

    update to relative paths in webpack

--- yelp/src/app.js
+++ yelp/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'font-awesome/css/font-awesome.css';
+// import 'font-awesome/css/font-awesome.css';
 import './app.css';
 import styles from './styles.module.css';
 
